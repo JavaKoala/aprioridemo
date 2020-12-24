1db834232d6f0c1e5112ea85869391a0b41064a0
Ben Meyer <bmeyer@Bens-Mac-mini.local>
2020-12-24 13:07:00 -0500

    Added document upload form

--- apriori/views.py
+++ apriori/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 from django.http import HttpResponse
-
+from .models import Document
+from .forms import UploadFileForm
 
 def index(request):
-    return render(request, 'apriori/index.html')
+    message = ''
+
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile=request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return redirect('index')
+        else:
+            message = 'The form is not valid. Fix the following error:'
+    else:
+        form = UploadFileForm()  # An empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    context = {'documents': documents, 'form': form, 'message': message}
+    return render(request, 'apriori/index.html', context)
