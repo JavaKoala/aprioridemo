2ad404bbb408094b00eda124792a2e48515278bc
Ben Meyer <bmeyer@Bens-Mac-mini.local>
2020-12-25 12:54:45 -0500

    Added bootstrap4

--- aprioridemo/settings.py
+++ aprioridemo/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'apriori.apps.AprioriConfig',
+    'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
