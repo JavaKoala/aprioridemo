35f6607edbfa0aba58c4aff1efe5f37c304ebd2b
Ben Meyer <bmeyer@Bens-Mac-mini.local>
2020-12-28 16:54:07 -0500

    Added view tests

--- apriori/tests.py
+++ apriori/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from .models import DataSet
 
-# Create your tests here.
+client = Client()
+
+def create_data_set():
+  with open('sample-datafiles/Market_Basket_Optimisation_min.csv') as fp:
+    client.post('/apriori/', {'datafile': fp})
+
+class ViewIndexTests(TestCase):
+  def test_get_no_datafiles(self):
+    response = client.get('/apriori/')
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "No documents.")
+    self.assertQuerysetEqual(response.context['data_sets'], [])
+
+  def test_get_datafiles(self):
+    create_data_set()
+    response = client.get('/apriori/')
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "Market_Basket_Optimisation_min")
+    self.assertQuerysetEqual(response.context['data_sets'], ['<DataSet: DataSet object (1)>'])
+
+  def test_post(self):
+    with open('sample-datafiles/Market_Basket_Optimisation.csv') as fp:
+      response = client.post('/apriori/', {'datafile': fp})
+    self.assertEqual(response.status_code, 302)
+
+class ViewShowTests(TestCase):
+  def test_get_datafile(self):
+    create_data_set()
+    response = client.get('/apriori/1/')
+    self.assertEqual(response.status_code, 200)
+    self.assertContains(response, "Market_Basket_Optimisation_min")
+
